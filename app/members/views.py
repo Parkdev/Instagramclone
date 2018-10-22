7f88f4fc71360cd4f0269d5674b54c4637c74ab0
Jeeho Park <devpark29@gmail.com>
2018-10-22 17:03:49 +0900

    Signupform에서 유효하지 않는 경우, 템플릿에서 해당 에러를 출력

--- app/members/views.py
+++ app/members/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
 from members.forms import LoginForm, SignupForm
+
 
 
 def login_view(request):
@@ -57,6 +60,34 @@ def logout_view(request):
 
 
 def signup_view(request):
+    """
+    render하는 경우
+    1. POST요청이며 사용자명이 이미 존재할 경우
+    2. POST요청이며 비밀번호가 같지 않는 경우
+    3. GET요청인 경우
+    redirect하는 경우
+    1. POST요청이며 사용자명이 존재하지 않고 비밀번호가 같은 경우
+
+    if request.method가 POST라면:
+        if 사용자명이 존재하면;
+            render1
+        if 비밀번호가 같지 않으면;
+            render2
+        (else, POST면서 사용자명도없고 비밀번호도 같으면):
+            redirect
+    (else, GET요청이면):
+        render
+
+    if request.method가 POST라면:
+        if 사용자명이 존재하면;
+        if 비밀번호가 같지 않으면;
+        (else, POST면서 사용자명도없고 비밀번호도 같으면):
+            return redirect
+    (POST면서 사용자명이 존재하면)
+    (POST면서 비밀번호가 같지않으면)
+    (POST면서 사용자명이 없고 비밀번호도 같은 경우가 "아니면" -> GET요청도 포함)
+    return render
+    """
     # URL: /members/signup/
     # Template: members/signup.html
     # Form:
@@ -66,11 +97,47 @@ def signup_view(request):
 
     # GET요청시 해당 템플릿 보여주도록 처리
     # base.html에 있는 'Signup'버튼이 이 쪽으로 이동할 수 있도록 url 링크걸기
+    form = SignupForm()
+    context = {'form': form,
+               'error': error}
     if request.method == 'GET':
-        form = SignupForm()
-        context = {
-            'form': form,
-        }
+
+        # form = SignupForm()
+        # context = {
+        #     'form': form,
+        # }
         return render(request, 'members/signup.html', context)
     else:
-        pass
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if User.objects.filter(username=username).exists():
+
+            error = f'{username}은 이미 사용중입니다.'
+
+
+        if password1 != password2 :
+
+            return HttpResponse('비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다.')
+
+        # create대신 create_user를사용하면 password를 해싱해줌
+        user = User.objects.create_user(
+            username=username,
+            password=password1,
+        )
+        login(request, user)
+        return redirect('posts:post-list')
+
+    return render(request, 'members/signup.html', context)
+
+
+        # 1. request.POST에 전달된 username, password1, password2를
+        #    각각 해당 이름의 변수에 할당
+        # 2-x에서는 HttpResponse에 문자열로 에러를 리턴해주기
+        #  2-1 username에 해당하는 User가 이미 있다면
+        #      사용자명 ({username})은 이미 사용중입니다.
+        #  2-2. password1과 password2가 일치하지 않는다면
+        #       비밀번호와 비밀번호 확인란의 값이 일치 하지 않습니다.
+        # 3. 위의 두 경우가 아니라면
+        #   새 User를 새엉, 해당 User로 로그인 시켜준 후 'post:post-list'로 redirect처리
