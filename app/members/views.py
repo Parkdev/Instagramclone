9951e3125dbf03116c0232bb74c7ea80c423ae1c
Jeeho Park <devpark29@gmail.com>
2018-10-22 17:09:18 +0900

    Signup view에서 중복 render()부분 제거

--- app/members/views.py
+++ app/members/views.py
@@ -97,39 +97,27 @@ def signup_view(request):
 
     # GET요청시 해당 템플릿 보여주도록 처리
     # base.html에 있는 'Signup'버튼이 이 쪽으로 이동할 수 있도록 url 링크걸기
-    form = SignupForm()
-    context = {'form': form,
-               'error': error}
-    if request.method == 'GET':
-
-        # form = SignupForm()
-        # context = {
-        #     'form': form,
-        # }
-        return render(request, 'members/signup.html', context)
-    else:
+    context = {
+        'form': SignupForm(),
+    }
+    if request.method == 'POST':
         username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
         if User.objects.filter(username=username).exists():
-
-            error = f'{username}은 이미 사용중입니다.'
-
-
-        if password1 != password2 :
-
-            return HttpResponse('비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다.')
-
-        # create대신 create_user를사용하면 password를 해싱해줌
-        user = User.objects.create_user(
-            username=username,
-            password=password1,
-        )
-        login(request, user)
-        return redirect('posts:post-list')
-
-    return render(request, 'members/signup.html', context)
+            context['error'] = f'사용자명({username})은 이미 사용중입니다.'
+        elif password1 != password2:
+            context['error'] = '비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다.'
+        else:
+            # create_user메서드는 create와 달리 자동으로 password해싱을 해줌
+            user = User.objects.create_user(
+                username=username,
+                password=password1,
+            )
+            login(request, user)
+            return redirect('posts:post-list')
+        return render(request, 'members/signup.html', context)
 
 
         # 1. request.POST에 전달된 username, password1, password2를
