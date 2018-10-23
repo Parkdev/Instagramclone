13727db8e45879d6cb9d6a89ea4d8883dc937a3f
Jeeho Park <devpark29@gmail.com>
2018-10-23 15:44:58 +0900

    Post create수정전

--- app/members/forms.py
+++ app/members/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 이 Form instance에 주어진 데이터가 유효하면
+        # authenticate에서 리턴돈 User객체를 체울 속성
+        self._user = None
     username = forms.CharField(
         # 일반 input[type=text]
         # form-control CSS클래스 사용
@@ -20,6 +26,24 @@ class LoginForm(forms.Form):
             }
         )
     )
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        if user is None:
+            raise forms.ValidationError('틀렸습니다.')
+        self._user = user
+
+    @property
+    # 유효성 검증을 실행했을 때 (is_valid())
+    # 만약 필드나 폼에서 유효하지 않은 항목이 있다면
+    # 이 부분에 추가
+    def user(self):
+        if self.errors:
+            raise ValueError('폼의 데이터 유효성 검증에 실패하였습니다.')
+        return self._user
+
 
 class SignupForm(forms.Form):
     #폼에 클래스를 추가해서 색을 변경하는등의 작업을 할 수 있다.
@@ -57,3 +81,5 @@ class SignupForm(forms.Form):
             password=self.cleaned_data['password1'],
         )
         return user
+
+
--- app/members/views.py
+++ app/members/views.py
@@ -20,30 +20,28 @@ def login_view(request):
     # LoginForm
     #  username, password를 받을 수 있도록 함
     #   password는 widget에 PasswordInput을 사용하기.
+
+    #username, password를 받는 부분
+    # LoginForm을 사용하도록 수정
+    # 로그인에 실패했을 경우, Template에 form.non_filed_errors를 사용해서
+    # 로그인에 실패했다는것을 출력\
+    context = {}
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None :
-            login(request, user)
+    #     username = request.POST['username']
+    #     password = request.POST['password']
+    #     user = authenticate(request, username=username, password=password)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            # 주어진 username, password로
+            # authenticate에 성공했다
+            # request와 authenticate된 user를 사용해서
+            # login()처리 후
+            login(request, form.user)
             return redirect('posts:post-list')
-
-
-        # 1. request.POST 에 데이터가 옴
-        # 2. 온 데이터 중에서 Username에 해당하는 값과 Password에 해당하는 값을 각각
-        #    username, password변수에 할당
-        # 3. 사용자 인증을 수행
-        #   username/password에 해당하는 사용자가 있는지 확인
-        # 4-1. 인증에 성공한다면
-        #   세션/쿠키 기반의 로그인 과정을 수행, 완료 후 posts:post-list페이지로 redirect
-        # 4-2. 인증에 실패한다면
-        #   이 페이지에서 인증에 실패했음을 사용자에게 알려줌
     else:
         form = LoginForm()
-        context = {
-            'form': form,
-        }
-        return render(request, 'members/login.html', context)
+    context['form'] = form
+    return render(request, 'members/login.html', context)
 
 def logout_view(request):
     # URL: /members/logout/
