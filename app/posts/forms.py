e4572ec10f1c377135d01c45f43835e3e01724b7
Jeeho Park <devpark29@gmail.com>
2018-10-23 16:51:27 +0900

    Post생성시 연결된 Comment도 선택적으로 생성, post_list에서 Post에 연결된 Comment들도 보여줌

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -30,4 +30,22 @@ class PostCreateForm(forms.Form):
             photo=self.cleaned_data['photo'],
             **kwargs,
         )
+
+
+
+        # 1. Post생성시 Comment생성 (선택적)
+        # 만약에 comment항목이 있다면
+        # 생성한 Post에 연결되는 Comment를 생성
+        # author=request.user 생성한 Post의 author와 동일
+        # post=post가 되도록, 생성한 Post
+
+        # 2. post_list에서 각 Post의 댓글 목록을 출력
+        comment_content = self.cleaned_data.get('comment')
+        if comment_content:
+
+            post.comments.create(
+                author=post.author,
+                content=comment_content,
+            )
+        
         return post
